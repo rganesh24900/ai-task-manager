e3c77ff73ddf7c7c5100dd95828c0d2f77975d4b
Ganesh <rganesh802@gmail.com>
2025-11-16 17:53:58 +0530

    fix:cors error

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,29 +1,36 @@
-import express from 'express'
-import cors from 'cors'
-import dotEnv from 'dotenv'
-import taskRoutes from './routes/tasks.routes'
-import authRoutes from './routes/auth.routes'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import cors from "cors";
+import dotEnv from "dotenv";
+import taskRoutes from "./routes/tasks.routes";
+import authRoutes from "./routes/auth.routes";
+import cookieParser from "cookie-parser";
 
 dotEnv.config();
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://ai-task-manager-two.vercel.app"
+];
+
 app.use(
     cors({
-        origin: [
-            "http://localhost:5173",
-            "https://ai-task-manager-two.vercel.app/"
-        ],
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                console.log("BLOCKED CORS:", origin);
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
         credentials: true,
     })
 );
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
+app.options("*", cors());
 
 app.use("/api/tasks", taskRoutes);
 app.use("/api/auth", authRoutes);
